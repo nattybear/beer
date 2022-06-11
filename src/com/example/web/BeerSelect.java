bb7339930cffbc7fbc5a5cde41e36cff4ad6319f
준규 <vpark45@gmail.com>
2022-06-11 12:40:01 +0900

    Servlet version 2

--- src/com/example/web/BeerSelect.java
+++ src/com/example/web/BeerSelect.java
@@ -1,8 +1,10 @@
 package com.example.web;
 
+import com.example.model.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import java.io.*;
+import java.util.*;
 
 public class BeerSelect extends HttpServlet {
 
@@ -14,6 +16,12 @@ public class BeerSelect extends HttpServlet {
     PrintWriter out = response.getWriter();
     out.println("Beer Selection Advice<br>");
     String c = request.getParameter("color");
-    out.println("<br>Got beer color " + c);
+
+    BeerExpert be = new BeerExpert();
+    List result = be.getBrands(c);
+    Iterator it = result.iterator();
+    while (it.hasNext()) {
+      out.print("<br>try: " + it.next());
+    }
   }
 }
